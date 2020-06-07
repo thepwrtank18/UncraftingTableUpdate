1d3ff1971c4d969823c15a629e37db45a8e7c341
thepwrtank18 <leoholdiman7@gmail.com>
2020-06-07 14:17:25 -0400

    Update UncraftingTable.java

--- content/src/main/java/com/thetank/uncrafting/UncraftingTable.java
+++ content/src/main/java/com/thetank/uncrafting/UncraftingTable.java
@@ -1,8 +1,6 @@
 package com.thetank.uncrafting;
 
-import net.minecraft.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -15,7 +13,8 @@ import org.apache.logging.log4j.Logger;
 @Mod("uncrafting")
 public class UncraftingTable
 {
-    private static final Logger LOGGER = LogManager.getLogger();
+    @SuppressWarnings("unused")
+	private static final Logger LOGGER = LogManager.getLogger();
 
     public UncraftingTable() 
     {
@@ -36,19 +35,8 @@ public class UncraftingTable
     }
 
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
+    public void onServerStarting(FMLServerStartingEvent event)
+    {
+    	
     }
 }
